b3fc1a78168530755b5eb667bc1a8de67deb40f7
tok124 <tim-levinsson@hotmail.com>
2015-06-28 18:12:01 +0200

    Added nav and loginform

--- includes/head.php
+++ includes/head.php
@@ -5,7 +5,7 @@
 Tim och Jasse Forum
 </title>
 <link rel="stylesheet" type="text/css" href="css/main.css">
-<script type="text/javascript" src=""></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <meta charset="UTF-8">
 <meta name="author" content="Tim Levinsson">
 <meta name="description" content="Forum">
---
+++ includes/nav.php
@@ -0,0 +1,28 @@
+<nav>
+	<ul>
+		<li><a href="#">Uno</a></li>
+		<li><a href="#">Dos</a></li>
+		<li><a href="#">Tres</a></li>
+		<li><a href="#">Quattro</a></li>
+		<li><a href="#">Register</a></li>
+		<li><a href="#" class="login">Login</a></li>
+	</ul>
+</li>
+</nav>
+<div id="loginbox">
+	<div id="loginform">
+		<div id="close" class="login"></div>
+		<div id="loginwrapper">
+			<form action="functions/login.php" method="POST">
+				<input type="text" name="user" placeholder="Username">
+				<input type="password" name="pass" placeholder="Password">
+				<center><input type="submit" value="Login"></center>
+			</form>
+		</div>
+	</div>
+</div>
+<script type="text/javascript">
+	$('.login').click(function(){
+		$('#loginbox').fadeToggle();
+	});
+</script>
